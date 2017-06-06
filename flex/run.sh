211c8f3a78ce2ee292805ebb39c3e6b199ba61a7
Kasra Koushan <kasrakoushan@khanacademy.org>
2017-06-06 10:06:17 -0700

    Re-structured directory, added a run script
    Summary: Added a script for running flex project, moved Models file
    
    Test Plan: Ran locally
    
    Reviewers: benkraft, csilvers
    
    Differential Revision: https://phabricator.khanacademy.org/D36387

---
+++ flex/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+export GOOGLE_APPLICATION_CREDENTIALS=./khan-cachetest-4e33955967d6.json
+export DATASTORE_DATASET=khan-cachetest
+export DATASTORE_EMULATOR_HOST=localhost:8081
+export DATASTORE_EMULATOR_HOST_PATH=localhost:8081/datastore
+export DATASTORE_HOST=http://localhost:8081
+export DATASTORE_PROJECT_ID=khan-cachetest
+export DATASTORE_USE_PROJECT_ID_AS_APP_ID=true
+source env/bin/activate
+python main.py
